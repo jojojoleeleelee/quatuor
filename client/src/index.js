2ef5664f3f34634691e0c9b216cbba609b996705
Joanne Lee <vernapii@gmail.com>
2018-09-25 09:54:34 -0700

    experimenting

--- client/src/index.js
+++ client/src/index.js
@@ -1,8 +1,10 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
+import { render } from 'react-dom'
+import { Provider } from 'react-redux'
 import App from './App';
-import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
-registerServiceWorker();
+render(
+  <Provider>
+    <App />
+  </Provider>,
+  document.getElementById('root'));
