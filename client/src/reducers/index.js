72b3fc6ff9947a9b1a0be44128cada2644d7adcc
Joanne Lee <vernapii@gmail.com>
2018-09-27 00:39:09 -0700

    clean up

--- client/src/reducers/index.js
+++ client/src/reducers/index.js
@@ -1,6 +1,6 @@
 import { combineReducers } from 'redux';
-import repertoireReducer from './repertoireReducer';
+import quartetReducer from './quartetReducer';
 
 export default combineReducers({
-  repertoire: repertoireReducer
+  quartet: quartetReducer
 })
