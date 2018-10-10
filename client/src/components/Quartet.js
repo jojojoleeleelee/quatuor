d7dd6faab50ddf8fba0387ec5bfcb6bcb76f201e
Joanne Lee <vernapii@gmail.com>
2018-10-10 10:35:08 -0700

    building show of individual;

--- client/src/components/Quartet.js
+++ client/src/components/Quartet.js
@@ -1,9 +1,9 @@
 import React from 'react';
 import { connect } from 'react-redux';
-
 const Quartet = (quartet) => {
   const learn_more = `${quartet.quartet.uri}`
   console.log(learn_more)
+
   return (
     <div className="card">
       <div className="card card-panel deep-purple lighten-4">
@@ -13,7 +13,7 @@ const Quartet = (quartet) => {
           <img className="responsive-img" src={quartet.quartet.image} alt="Smiley face" height="390" width="390" />
           <h3><strong>{quartet.quartet.title}</strong></h3>
           <h4>{quartet.quartet.year} - {quartet.quartet.country}</h4>
-          <a href={learn_more}>Learn More!</a>
+          <a target="_blank" href={learn_more}>Learn More!</a>
           <br />
         </div>
       </div>
